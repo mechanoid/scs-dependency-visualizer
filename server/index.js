3d58b8a497032aecb00017f1bef4b1d3c36271cf
Falk Hoppe <falkhoppe81@gmail.com>
2019-04-02 10:42:08 +0200

    allow loading component definitions by HTTP

--- server/index.js
+++ server/index.js
@@ -4,34 +4,34 @@ const renderer = require('./lib/renderer.js')
 const resourceRenderer = require('./lib/resource-renderer.js')
 const componentLoader = require('./lib/component-loader.js')
 
-const targetComponentsPath = defaultPath => req =>
-  req.query['components-path'] || defaultPath
-
-module.exports = defaultComponentDefinitionPath => {
+module.exports = componentDefinition => {
   const app = require('./lib/app.js').app
-  const componentsPath = targetComponentsPath(defaultComponentDefinitionPath)
 
-  app.get('/', (req, res) => {
-    const { components, clients, connectionTypes } = componentLoader(
-      componentsPath(req)
-    )
+  app.get('/', async (req, res, next) => {
+    try {
+      const { components, clients, connectionTypes } = await componentLoader(
+        componentDefinition
+      )
 
-    const { client, connections, filterComponents } = req.query
-    const hideFilters = req.query['hide-filters']
-    const filters = { client, connections, filterComponents }
-    filters.client = filters.client || clients[0]
-    const rendered = renderer(components, filters)
+      const { client, connections, filterComponents } = req.query
+      const hideFilters = req.query['hide-filters']
+      const filters = { client, connections, filterComponents }
+      filters.client = filters.client || clients[0]
+      const rendered = renderer(components, filters)
 
-    res.render('index.pug', {
-      clients,
-      connectionTypes,
-      rendered,
-      filters,
-      components,
-      hideFilters,
-      appPath: app.path(),
-      req
-    })
+      res.render('index.pug', {
+        clients,
+        connectionTypes,
+        rendered,
+        filters,
+        components,
+        hideFilters,
+        appPath: app.path(),
+        req
+      })
+    } catch (e) {
+      next(e)
+    }
   })
 
   const componentsWithResources = c => {
@@ -41,32 +41,38 @@ module.exports = defaultComponentDefinitionPath => {
       .filter(isNotEmptyArray)
   }
 
-  app.get('/resources', (req, res) => {
-    const { components, syncConnections } = componentLoader(componentsPath(req))
+  app.get('/resources', async (req, res, next) => {
+    try {
+      const { components, syncConnections } = await componentLoader(
+        componentDefinition
+      )
 
-    const { selectedComponent } = req.query
-    const hideFilters = req.query['hide-filters']
-    const resourceComponents =
-      components
-        .reduce((memo, c) => memo.concat(componentsWithResources(c)), [])
-        .filter(isNotEmptyArray) || []
+      const { selectedComponent } = req.query
+      const hideFilters = req.query['hide-filters']
+      const resourceComponents =
+        components
+          .reduce((memo, c) => memo.concat(componentsWithResources(c)), [])
+          .filter(isNotEmptyArray) || []
 
-    const component = selectedComponent
-      ? resourceComponents.find(c => c.name === selectedComponent)
-      : resourceComponents[0]
+      const component = selectedComponent
+        ? resourceComponents.find(c => c.name === selectedComponent)
+        : resourceComponents[0]
 
-    const rendered = component
-      ? resourceRenderer(resourceComponents, component, syncConnections)
-      : ''
+      const rendered = component
+        ? resourceRenderer(resourceComponents, component, syncConnections)
+        : ''
 
-    res.render('resource.pug', {
-      rendered,
-      selectedComponent,
-      resourceComponents,
-      hideFilters,
-      appPath: app.path(),
-      req
-    })
+      res.render('resource.pug', {
+        rendered,
+        selectedComponent,
+        resourceComponents,
+        hideFilters,
+        appPath: app.path(),
+        req
+      })
+    } catch (e) {
+      next(e)
+    }
   })
 
   return {
--- server/lib/component-loader.js
+++ server/lib/component-loader.js
@@ -1,8 +1,23 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
+const fetch = require('node-fetch')
 
-module.exports = componentDefinitionPath => {
-  const data = yaml.safeLoad(fs.readFileSync(componentDefinitionPath, 'utf8'))
+const resolve = async componentDefinition =>
+  fetch(componentDefinition.uri).then(res => {
+    if (res.ok) {
+      return res
+    }
+
+    throw new Error(
+      `'Component Definition could not be loaded. Status: ${res.status}`
+    )
+  })
+
+module.exports = async componentDefinition => {
+  const data =
+    typeof componentDefinition === 'string'
+      ? yaml.safeLoad(fs.readFileSync(componentDefinition, 'utf8'))
+      : await resolve(componentDefinition)
 
   const components = (data.components || []).map(c => {
     c.filterable = true // only root level components can be filtered out
